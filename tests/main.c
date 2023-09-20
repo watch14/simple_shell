36a49ff8922bfdfe3c56c10aa3bac88b1c4704b5
YusufYuso <yusuf.elsaed@gmail.com>
2023-09-20 17:57:46 +0300

    Adding C source, ASCII text file: tests/main.c

---
+++ tests/main.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the simple shell program
+ * @argc: Number of command-line arguments (unused)
+ * @argv: Array of command-line argument strings (unused)
+ * @env: Array of environment variables
+ *
+ * Return: Always 0 (successful execution)
+ */
+
+int main(int argc, char **argv, char **env)
+{
+	char *buf;
+	size_t n = 0;
+	int ln = 1;
+
+
+	(void)argc;
+	(void)argv;
+
+	buf = NULL;
+
+	write(1, "$ ", 2);
+	getline(&buf, &n, stdin);
+
+	while (buf)
+	{
+		if (strcmp(buf, "exit\n") == 0)
+			break;
+
+		execute(buf, env, ln);
+		ln++;
+
+		free(buf);
+		buf = NULL;
+		write(1, "$ ", 2);
+		getline(&buf, &n, stdin);
+	}
+	return (0);
+}
