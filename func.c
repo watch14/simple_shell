e634fe468b43573d5a17148edc4dfd7ece7cee51
YusufYuso <yusuf.elsaed@gmail.com>
2023-09-20 15:17:52 +0300

    Updating C source, ASCII text file: func1.c

--- func.c
+++ func.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * get_env - Get the value of an environment variable.
  * @env_var: The name of the environment variable to retrieve.
@@ -72,64 +71,13 @@ void handle_special(char *arg)
 	for (k = 0; k < len; k++)
 	{
 		if (arg[k] == '"' || arg[k] == '\'' ||
-				arg[k] == '`' || arg[k] == '\\' ||
-				arg[k] == '*' || arg[k] == '&' || arg[k] == '#')
+			arg[k] == '`' || arg[k] == '\\' ||
+			arg[k] == '*' || arg[k] == '&' || arg[k] == '#')
 		{
 			memmove(arg + k + 1, arg + k, len - k + 1);
 			arg[k] = '\\';
 			len++;
 			k++;
 		}
-	}
-}
-
-/**
- * execute - Execute a command in a new process.
- * @command: The command to execute.
- * @env: The environment variables array.
- * @line_number: The line number of the command in the shell script.
- */
-
-void execute(char *command, char **env, int line_number)
-{
-	char *argv[20];
-	int i = 0, j;
-	char *token = strtok(command, " \t\n");
-
-	pid_t pid;
-
-	while (token != NULL)
-	{
-		argv[i++] = token;
-		token = strtok(NULL, " \t\n");
-	}
-	argv[i] = NULL;
-
-	for (j = 0; j < i; j++)
-		handle_special(argv[j]);
-
-	pid = fork();
-
-	if (pid == -1)
-	{
-		perror("fork");
-	}
-	else if (pid == 0)
-	{
-		char exe[50];
-
-		add_path(argv[0], exe, env);
-		if (exe[0] == '\0')
-		{
-			fprintf(stderr, "hsh: %d: %s: not found\n", line_number, argv[0]);
-			exit(1);
-		}
-		execve(exe, argv, env);
-		fprintf(stderr, "hsh: %d: %s: not found\n", line_number, argv[0]);
-		exit(1);
-	}
-	else
-	{
-		wait(NULL);
 	}
 }
